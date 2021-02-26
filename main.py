dee295f2012e07f42ee10c7fd77aad1608cac373
falsepysche <tanner@tanner.ninja>
2021-02-25 21:00:28 -0500

    tab fix

--- main.py
+++ main.py
@@ -22,25 +22,25 @@ word_count_base = 0
 word_count_tele = 0
 
 for english_word in english_dict:
-  # print(english_word)
-  	for word in substring_list:
-		# print(english_word, word)
-		index_found = english_word.find(word)
-		if word == english_word or index_found > -1:
-			word_count_thing = word_count_thing + 1
-			print('match: ', word, english_word)
-			if word == 'shit': 
-				word_count_shit = word_count_shit + 1
-			if word == 'ass':
-				word_count_ass = word_count_ass + 1
-			if word == 'jam':
-				word_count_jam = word_count_jam + 1
-			if word == 'base':
-				word_count_base = word_count_base + 1
-			if word == 'tele':
-				word_count_tele = word_count_tele + 1
-	# else:
-	  # print(word, ' does not match', english_word)
+	# print(english_word)
+		for word in substring_list:
+			# print(english_word, word)
+			index_found = english_word.find(word)
+			if word == english_word or index_found > -1:
+				word_count_thing = word_count_thing + 1
+				print('match: ', word, english_word)
+				if word == 'shit': 
+					word_count_shit = word_count_shit + 1
+				if word == 'ass':
+					word_count_ass = word_count_ass + 1
+				if word == 'jam':
+					word_count_jam = word_count_jam + 1
+				if word == 'base':
+					word_count_base = word_count_base + 1
+				if word == 'tele':
+					word_count_tele = word_count_tele + 1
+		# else:
+		# print(word, ' does not match', english_word)
 print(word_count_thing)
 print()
 print('shit: ', word_count_shit)
