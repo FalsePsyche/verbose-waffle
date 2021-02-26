cb99db6f42aa54802c14c18c08110ee801748274
Derek <Dborne10@gmail.com>
2021-02-25 20:57:56 -0500

    count words of substring list, yo fix this shit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import nltk
-# nltk.download()
+#nltk.download()
 from nltk.corpus import words
 english_dict = words.words()
 print(len(english_dict))
@@ -14,19 +14,42 @@ print("find all the words in the dictionary that have swears as substrings")
 print()
 # todo put words in list
 test_list = ['a', 'b', 'c', 'jammy', 'baseball', 'tel', 'tele']
-word_list = ['shit', 'ass', 'jam', 'base', 'tele']
+substring_list = ['shit', 'ass', 'jam', 'base', 'tele']
+word_count_shit = 0
+word_count_ass = 0
+word_count_jam = 0
+word_count_base = 0
+word_count_tele = 0
 
 for english_word in english_dict:
   # print(english_word)
-  for word in word_list:
-    # print(english_word, word)
-    index_found = english_word.find(word)
-    if word == english_word or index_found > -1:
-      print('match: ', word, english_word)
-    # else:
-      # print(word, ' does not match', english_word)
-
-# todo count number of matches per word in word_list
+  	for word in substring_list:
+		# print(english_word, word)
+		index_found = english_word.find(word)
+		if word == english_word or index_found > -1:
+			word_count_thing = word_count_thing + 1
+			print('match: ', word, english_word)
+			if word == 'shit': 
+				word_count_shit = word_count_shit + 1
+			if word == 'ass':
+				word_count_ass = word_count_ass + 1
+			if word == 'jam':
+				word_count_jam = word_count_jam + 1
+			if word == 'base':
+				word_count_base = word_count_base + 1
+			if word == 'tele':
+				word_count_tele = word_count_tele + 1
+	# else:
+	  # print(word, ' does not match', english_word)
+print(word_count_thing)
+print()
+print('shit: ', word_count_shit)
+print('ass: ', word_count_ass)
+print('jam: ', word_count_jam)
+print('base: ', word_count_base)
+print('tele: ', word_count_base)
+# todo count number of matches per word in substring_list
+	
 # todo walk through setup of git repo, then github
 
 # print(word_list)
