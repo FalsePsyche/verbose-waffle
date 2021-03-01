a8a5a61fa73d1632b46c120b57f3a0b25aa31035
Derek <Dborne10@gmail.com>
2021-02-28 22:41:35 -0500

    works

--- main.py
+++ main.py
@@ -14,13 +14,16 @@ print("find all the words in the dictionary that have swears as substrings")
 print()
 # todo put words in list
 test_list = ['a', 'b', 'c', 'jammy', 'baseball', 'tel', 'tele']
-substring_list = ['shit', 'ass', 'jam', 'base', 'tele']
+substring_list = ['shit', 'ass', 'jam', 'base', 'tele', 'damn']
 word_count_thing = 0
 word_count_shit = 0
 word_count_ass = 0
 word_count_jam = 0
 word_count_base = 0
 word_count_tele = 0
+word_count_damn = 0
+
+substring_count_list = [0] * len(substring_list)
 
 for english_word in english_dict:
 	# print(english_word)
@@ -30,6 +33,16 @@ for english_word in english_dict:
 			if word == english_word or index_found > -1:
 				word_count_thing = word_count_thing + 1
 				print('match: ', word, english_word)
+				match_number = 0
+				for match in substring_list:
+					print(match_number)
+					if word == match:
+						substring_count_list[match_number] +=1 
+						print(substring_count_list)
+					match_number +=1
+					#print('incremented match_number: ', match_number)
+						
+
 				if word == 'shit': 
 					word_count_shit = word_count_shit + 1
 				if word == 'ass':
@@ -40,6 +53,8 @@ for english_word in english_dict:
 					word_count_base = word_count_base + 1
 				if word == 'tele':
 					word_count_tele = word_count_tele + 1
+				if word == 'damn':
+					word_count_damn = word_count_damn + 1
 		# print(word, ' does not match', english_word)
 print(word_count_thing)
 print()
@@ -48,12 +63,14 @@ print('ass: ', word_count_ass)
 print('jam: ', word_count_jam)
 print('base: ', word_count_base)
 print('tele: ', word_count_tele)
-assert(word_count_shit + word_count_ass + word_count_jam + word_count_base + word_count_tele == word_count_thing)
+print('damn: ', word_count_damn)
+assert(word_count_shit + word_count_ass + word_count_jam + word_count_base + word_count_tele + word_count_damn == word_count_thing)
 
-# todo count number of matches per word in substring_list
-	
-# todo walk through setup of git repo, then github
+#TODO add item to substring list
 
+print(type(substring_count_list))
+print(substring_count_list)
+assert(sum(substring_count_list)==word_count_thing)
 # print(word_list)
 # for word in word_list:
 #   print(word)
