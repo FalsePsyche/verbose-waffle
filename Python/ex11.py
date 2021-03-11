0f84833a4e36bb7ddecc3b677cc55112d61b8f6e
Derek <Dborne10@gmail.com>
2021-03-11 18:44:41 -0500

    Python ex 8-14

---
+++ Python/ex11.py
@@ -0,0 +1,11 @@
+print("How old are you?", end=' ')
+age = input()
+print("How tall are you?", end=' ')
+height = input()
+print("How much do you weigh?", end=' ')
+weight = input()
+
+cookies = input('What type of cookies do you like? ')
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+print('and you love ' + cookies + ' cookies.')
