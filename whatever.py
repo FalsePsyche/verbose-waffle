e371f0b4d5086ba611a20838874926680cbbe9d8
Derek <Dborne10@gmail.com>
2021-03-12 20:59:07 -0500

    format

--- whatever.py
+++ whatever.py
@@ -1,11 +1,11 @@
+from nltk.corpus import words
+import json
+import nltk
 variable_name_1 = {'variable_a': 2, 'variable_b': 7}
 print(variable_name_1)
-print(variable_name_1 ['variable_a'])
+print(variable_name_1['variable_a'])
 
-import nltk
-import json
 # nltk.download()
-from nltk.corpus import words
 english_dict = words.words()
 print(len(english_dict))
 print("find all the words in the dictionary that have swears as substrings")
@@ -13,15 +13,15 @@ print("Derek smeels")
 print()
 # List of words to match
 matches = {'shit': [],
-			'ass': [],
-			'jam': [],
-			'base': [],
-			'tele': [],
-			'damn': [],
-			'wack': [],
-			'dawg': [],
-			'nut': [] }
-matches ['word'] = []
+           'ass': [],
+           'jam': [],
+           'base': [],
+           'tele': [],
+           'damn': [],
+           'wack': [],
+           'dawg': [],
+           'nut': []}
+matches['word'] = []
 word_count_thing = 0
 for english_word in english_dict:
     # print(english_word)
@@ -30,15 +30,16 @@ for english_word in english_dict:
         index_found = english_word.find(word)
         if word == english_word or index_found > -1:
             word_count_thing = word_count_thing + 1
-            #print(matches[word])
-            matches [word].append(english_word)
-            match_number = 0           
+            # print(matches[word])
+            matches[word].append(english_word)
+            match_number = 0
 print(word_count_thing)
 print()
 print(list(matches.keys()))
-print(json.dumps(matches, indent= 4))
+print(json.dumps(matches, indent=4))
 for word in list(matches.keys()):
-	print(word, len(matches [word]))
-assert(word_count_thing == sum([len(value_list) for value_list in list(matches.values())]))
+    print(word, len(matches[word]))
+assert(word_count_thing == sum([len(value_list)
+                                for value_list in list(matches.values())]))
 with open('data.json', 'w+') as file:
-	file.write(json.dumps(matches, indent = 4))
+    file.write(json.dumps(matches, indent=4))
